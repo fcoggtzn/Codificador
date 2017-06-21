c3344a1c47ce415eca5d2b662d2833d1207f54eb
Francisco Gutierrez <fco@ovante.com.mx>
2017-06-21 15:37:26 -0500

    Librerias fop

--- Codificador-ejb/src/java/ejb/Firma.java
+++ Codificador-ejb/src/java/ejb/Firma.java
@@ -6,26 +6,21 @@
 package ejb;
 
 import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.GeneralSecurityException;
 import java.security.InvalidKeyException;
 
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.Signature;
 import java.security.SignatureException;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
+import org.apache.commons.ssl.PKCS8Key;
 
 /**
  *
@@ -42,86 +37,29 @@ public class Firma implements FirmaLocal {
             
             ClassLoader loader = Firma.class.getClassLoader(); 
            
-         /*   String certPath = "mycert.pem";
-            File pubCertFile = new File(certPath);*/
+       
             BufferedInputStream bis = null;
-      //      InputStream certResource = loader.getResourceAsStream("../../resources/"+rfc+"/"+rfc+".cer");
             InputStream keyResource = loader.getResourceAsStream("resources/"+rfc+"/"+rfc+".key");
 
-            /*
-                // bis = new BufferedInputStream(new FileInputStream(pubCertFile));
-                bis = new BufferedInputStream(certResource);
-            
-            CertificateFactory certFact = null;
-            Certificate cert = null;
-            try {
-                certFact = CertificateFactory.getInstance("X.509");
-                cert = certFact.generateCertificate(bis);
-            } catch (CertificateException e) {
-                throw new Exception("Could not instantiate cert", e);
-            }
-            bis.close();
-            ArrayList<Certificate> certs = new ArrayList<Certificate>();
-            certs.add(cert);
-*/
-            /*String keyPath = "mykey.pem";
-            File privKeyFile = new File(keyPath);*/
-            /*try {
-                bis = new BufferedInputStream(new FileInputStream(privKeyFile));
-              
-            } catch (FileNotFoundException e) {
-                throw new Exception("Could not locate keyfile at '" + keyPath + "'", e);
-            }*/
             bis = new BufferedInputStream(keyResource);
 
-          /*  byte[] privKeyBytes = new byte[(int) privKeyFile.length()];*/
             byte[] privKeyBytes = new byte[keyResource.available()];
             bis.read(privKeyBytes);
             bis.close();
-            KeyFactory keyFactory;
-            //= KeyFactory.getInstance("RSA");
-            
-            KeySpec ks;
-            
-            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(KeyBytes); 
-            RSAPrivateKey privKey; 
-             
-            try {
-                keyFactory = KeyFactory.getInstance("RSA");
-                privateKey = keyFactory.generatePrivate(keySpec);
-                algorithm = keyFactory.getAlgorithm();
-                //algorithm = "RSA";
-                //publicKey = keyFactory.generatePublic(keySpec);
-            } catch (InvalidKeySpecException excep1) {
-                try {
-                    KeyFactory keyFactory = KeyFactory.getInstance("DSA");
-                    privateKey = keyFactory.generatePrivate(keySpec);
-                    algorithm = keyFactory.getAlgorithm();
-                    //publicKey = keyFactory.generatePublic(keySpec);
-                } catch (InvalidKeySpecException excep2) {
-
-                    KeyFactory keyFactory = KeyFactory.getInstance("EC");
-                    privateKey = keyFactory.generatePrivate(keySpec);
-
-                } // inner catch
-            }
-            
-            
-            
-            ks = new PKCS8EncodedKeySpec(privKeyBytes);
-            
-            
-            
-                                
-            privKey = (RSAPrivateKey) keyFactory.generatePrivate(ks);
-            
-
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+               PKCS8Key pkcs8;
+            pkcs8 = new PKCS8Key(privKeyBytes, "12345678a".toCharArray());
+        PrivateKey privKey = pkcs8.getPrivateKey();       
+        
+        
             Signature signature = Signature.getInstance("SHA1withRSA");
             signature.initSign(privKey);
             signature.update(cadenaOriginal);
             return signature.sign();
         } catch (IOException | InvalidKeyException | NoSuchAlgorithmException | SignatureException | InvalidKeySpecException e) {
             System.out.println(e);
+        } catch (GeneralSecurityException ex) {
+            Logger.getLogger(Firma.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
